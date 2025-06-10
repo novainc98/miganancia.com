03884fcbcc1c69f9365c0c9d64deac9bbe00ac7f
novainc98 <novainc98@gmail.com>
2025-06-10 16:01:32 -0600

    inputs rojos si están vacios v2

--- script.js
+++ script.js
@@ -30,13 +30,26 @@ function navegacionConEnter(listaDeElementos) {
     });
 }
 
+function cadenaVacio(cadena) {
+    return cadena.length < 4 || !isNaN(cadena);
+}
+
+function noEsNumero (numero){
+    return isNaN(numero);
+}
+
 // === Eventos
 calcularBtn.addEventListener("click", function () {
+<<<<<<< Updated upstream
     const nombre = document.getElementById("inp-nombre").value;
+=======
+    const nombreProducto = document.getElementById("inp-nombre").value;
+>>>>>>> Stashed changes
     const precio = parseFloat(document.getElementById("inp-precio").value);
     const cantidad = parseInt(document.getElementById("inp-cantidad").value)
     const precioVenta = parseFloat(document.getElementById("inp-precio-venta").value);
 
+<<<<<<< Updated upstream
     const campos = document.querySelectorAll("input");
     campos.forEach(campo => campo.classList.remove("error"));
 
@@ -46,6 +59,41 @@ calcularBtn.addEventListener("click", function () {
         if (isNaN(cantidad)) document.getElementById("inp-cantidad").classList.add("error");
         if (isNaN(precioVenta)) document.getElementById("inp-precio-venta").classList.add("error");
         alert('Los campos deben estar completos');
+=======
+    const entradas = document.querySelectorAll("input");
+    entradas.forEach(entrada => entrada.classList.remove("error"));
+
+    if (cadenaVacio(nombreProducto) || isNaN(precio) || isNaN(cantidad) || isNaN(precioVenta)) {
+        if (cadenaVacio(nombreProducto)) {
+            const inputNombreProducto = document.getElementById("inp-nombre");
+            inputNombreProducto.classList.add("error");
+            inputNombreProducto.value = "";
+            inputNombreProducto.placeholder = "..."
+        }
+
+        if(noEsNumero(precio)){
+            const inputPrecio = document.getElementById("inp-precio");
+            inputPrecio.classList.add("error");
+            inputPrecio.value = "";
+            inputPrecio.placeholder = "..."
+        }
+
+         if(noEsNumero(cantidad)){
+            const inputPrecio = document.getElementById("inp-cantidad");
+            inputPrecio.classList.add("error");
+            inputPrecio.value = "";
+            inputPrecio.placeholder = "..."
+        }
+
+          if(noEsNumero(precioVenta)){
+            const inputPrecio = document.getElementById("inp-precio-venta");
+            inputPrecio.classList.add("error");
+            inputPrecio.value = "";
+            inputPrecio.placeholder = "..."
+        }
+
+        alert("Eror ingresa la informaicón correcta")
+>>>>>>> Stashed changes
         return;
     }
 
@@ -56,14 +104,23 @@ calcularBtn.addEventListener("click", function () {
 
 
     document.getElementById("resultados").innerHTML =
+<<<<<<< Updated upstream
     `
     Nombre: ${nombre}<br>
+=======
+        `
+    Nombre del producto: ${nombreProducto} <br>     
+>>>>>>> Stashed changes
     Costo unitario: $${Math.round(costoUnitaro * 100) / 100} <br>
     Ganancia Unitaria: $${Math.round(gananciaUnitaria * 100) / 100} <br>
     Ganancia Total $${Math.round(gananciaTotal * 100) / 100}
     `
 
+<<<<<<< Updated upstream
     console.log({nombre});
+=======
+    console.log({nombreProducto});
+>>>>>>> Stashed changes
     console.log({ costoTotal });
     console.log({ costoUnitaro });
     console.log({ gananciaUnitaria });
